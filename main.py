499ea10b8e824726b2fc8f7b4e4b2e734e9d222f
Fajar Tri Haryanto <ftfajar5@gmail.com>
2021-06-15 07:42:44 +0900

    still figuring out what update.message is

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-import os
+import os, re
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
 
+updater = Updater(os.environ["BOT_TOKEN"])
+
+channel_id = "@GaleriBlenderIndonesia"
+group_id = "@Blender3DIndonesia"
 
 def hello(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
+# forward from
+# reply_to = Update.message.reply_to_message
+# reply_chat_id = reply_to.from.id
 
-updater = Updater(os.environ["BOT_TOKEN"])
+# if re.search('caption', reply_to):
+#     reply_caption = reply_to.caption
+# else:
+#     reply_caption = ""
+
 
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
 
